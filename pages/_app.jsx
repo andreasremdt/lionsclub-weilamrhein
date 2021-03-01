4a1d4bfcacb2f095604b13314c24119e71eef8c0
Andreas Remdt <me@andreasremdt.com>
2021-03-01 14:50:30 +0100

    feat: add base layout and navigation

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,28 @@
 import PropTypes from 'prop-types';
+import Head from 'next/head';
+import Navigation from '../components/navigation';
 import '../styles/globals.css';
 
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap"
+        />
+      </Head>
+      <Navigation />
+      <main className="container">
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
 
 App.propTypes = {
-  Component: PropTypes.node.isRequired,
+  Component: PropTypes.elementType.isRequired,
   pageProps: PropTypes.object,
 };
 
